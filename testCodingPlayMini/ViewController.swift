d4626417b88035e9505b5a12ef02b5162ec902e2
Nova Arisma <novaarisma95@gmail.com>
2019-11-11 14:30:17 +0700

    Initial Commit

---
+++ testCodingPlayMini/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testCodingPlayMini
+//
+//  Created by Nova Arisma on 11/11/19.
+//  Copyright © 2019 Nova Arisma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
